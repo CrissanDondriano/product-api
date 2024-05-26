1c1cb3d4c78efd62c47205d876c1ed1410e27a14
Crissan Dondriano <dondrianocrissan20@gmail.com>
2024-05-26 23:46:22 +0800

    adding column for table

--- database/migrations/2024_05_26_154247_create_products_table.php
+++ database/migrations/2024_05_26_154247_create_products_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('product_name');
+            $table->text('product_description');
+            $table->integer('price');
+            $table->integer('quantity');
             $table->timestamps();
         });
     }
