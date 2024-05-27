811290e7fe76130cf6b6c2a3efcdcd10294c53b8
Crissan Dondriano <dondrianocrissan20@gmail.com>
2024-05-27 22:34:03 +0800

    adding route for the api for CRUD in controller User

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 
 
-//Route::post('/login', [AuthController::class, 'login']);
-//Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 
 Route::get('/products', [ProductController::class, 'index']);
 Route::post('/newProducts', [ProductController::class, 'store']);
@@ -15,3 +16,11 @@ Route::get('/getProducts/{id}', [ProductController::class, 'show']);
 Route::put('/update/Products/{id}', [ProductController::class, 'update']);
 Route::delete('/deleteProducts/{id}', [ProductController::class, 'destroy']);
 Route::get('/searchProducts/{title}', [ProductController::class, 'search']);
+
+
+Route::get('/user', [UserController::class, 'index']);
+Route::post('/newUser', [UserController::class, 'store']);
+Route::get('/getUser/{id}', [UserController::class, 'show']);
+Route::put('/update/User/{id}', [UserController::class, 'update']);
+Route::delete('/deleteUser/{id}', [UserController::class, 'destroy']);
+Route::get('/searchUser/{title}', [UserController::class, 'search']);
